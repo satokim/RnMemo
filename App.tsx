db31ee7dd4ec4e2823385a6e1b09fd5c7e44695c
satokim <usksty@gmail.com>
2024-09-26 23:27:16 +0900

    Install ESLint

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = (): JSX.Element => {
   return (
     <View style={styles.container}>
       <Text>Aora!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+
+export default App
