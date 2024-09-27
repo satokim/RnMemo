81fad82d6fef1ef7259370794217fe705cdec087
satokim <usksty@gmail.com>
2024-09-27 23:30:05 +0900

    Insert 'expo-router'

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hi Yoyo</Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({
+    
+})
