26a5ffdd9bd59c4abb00ec9b7cefdc9070b0e5a7
satokim <usksty@gmail.com>
2024-09-28 00:26:15 +0900

    add : add styles in memo list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,57 +5,39 @@ const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
 
-      <View>
-        <View>
-          <Text>rn memo</Text>
-          <Text>Logout</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>rn memo</Text>
+          <Text style={styles.headerRight}>Logout</Text>
         </View>
       </View>
 
       <View>
 
-        <View>
+        <View style={styles.listItem}>
           <View>
-            <View>
-              <Text>LIST 🔓 [A]</Text>
-              <Text>22.11.1995</Text>
-            </View>
+            <Text style={styles.listItemTitle}>LIST 🔓 [A]</Text>
+            <Text style={styles.listItemDate}>22.11.1995</Text>
           </View>
           <View>
             <Text>x</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.listItem}>
           <View>
-            <View>
-              <Text>LIST 🔓 [A]</Text>
-              <Text>22.11.1995</Text>
-            </View>
+            <Text style={styles.listItemTitle}>LIST 🔓 [A]</Text>
+            <Text style={styles.listItemDate}>22.11.1995</Text>
           </View>
           <View>
             <Text>x</Text>
           </View>
         </View>
 
-        <View>
+        <View style={styles.listItem}>
           <View>
-            <View>
-              <Text>LIST 🔓 [A]</Text>
-              <Text>22.11.1995</Text>
-            </View>
-          </View>
-          <View>
-            <Text>x</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <View>
-              <Text>LIST 🔓 [A]</Text>
-              <Text>22.11.1995</Text>
-            </View>
+            <Text style={styles.listItemTitle}>LIST 🔓 [A]</Text>
+            <Text style={styles.listItemDate}>22.11.1995</Text>
           </View>
           <View>
             <Text>x</Text>
@@ -64,8 +46,8 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <View>
-        <Text>✚</Text>
+      <View style={styles.addBtn}>
+        <Text style={styles.addTitle}>✚</Text>
       </View>
 
     </View>
@@ -77,7 +59,69 @@ export default Index
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: '#rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  listItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)'
+  },
+  listItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  listItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
+  },
+  addBtn: {
+    height: 64,
+    width: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    // iOS
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 8},
+    // Android
+    elevation: 8
+  },
+  addTitle: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 45
   }
 })
