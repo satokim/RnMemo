008eee10629b3e07797251282a32d2055f55eb2e
satokim <usksty@gmail.com>
2024-09-27 23:46:02 +0900

    Insert 'markup screen'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,71 @@ import React from 'react'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hi Yoyo</Text>
+    <View style={styles.container}>
+
+      <View>
+        <View>
+          <Text>rn memo</Text>
+          <Text>Logout</Text>
+        </View>
+      </View>
+
+      <View>
+
+        <View>
+          <View>
+            <View>
+              <Text>LIST 🔓 [A]</Text>
+              <Text>22.11.1995</Text>
+            </View>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <View>
+              <Text>LIST 🔓 [A]</Text>
+              <Text>22.11.1995</Text>
+            </View>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <View>
+              <Text>LIST 🔓 [A]</Text>
+              <Text>22.11.1995</Text>
+            </View>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <View>
+              <Text>LIST 🔓 [A]</Text>
+              <Text>22.11.1995</Text>
+            </View>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>✚</Text>
+      </View>
+
     </View>
   )
 }
@@ -12,5 +75,9 @@ const Index = (): JSX.Element => {
 export default Index
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
